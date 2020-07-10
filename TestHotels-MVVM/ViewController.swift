dfac3e46af5cf32f147780145540d269758485cf
Jesterix <ghaidenko@gmail.com>
2020-07-10 11:47:50 +0300

    Initial Commit

---
+++ TestHotels-MVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestHotels-MVVM
+//
+//  Created by Georgy Khaydenko on 10.07.2020.
+//  Copyright © 2020 Georgy Khaydenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
