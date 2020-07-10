8746e2feabf04cb5388599a8d25eac2588a8945a
Jesterix <ghaidenko@gmail.com>
2020-07-10 15:41:53 +0300

    add DetailViewModel

---
+++ TestHotels-MVVM/Main/Detail/DetailViewModel.swift
@@ -0,0 +1,45 @@
+import Bond
+import UIKit
+
+final class DetailViewModel {
+    let details = Observable<HotelDetails?>(nil)
+    let image = Observable<UIImage>(UIImage())
+    let hotel: Hotel
+    let error = Observable<Error?>(nil)
+    let refreshing = Observable<Bool>(false)
+    
+    private let dataManager: NetworkManager
+    
+    init(
+        dataManager: NetworkManager,
+        hotel: Hotel
+    ) {
+        self.dataManager = dataManager
+        self.hotel = hotel
+    }
+    
+    func fetch() {
+        refreshing.value = true
+        
+        dataManager.getHotelDetails(for: String(hotel.id)) { result in
+            switch result {
+            case .success(let response):
+                self.details.value = response
+            case .failure(let error):
+                self.error.value = error
+            }
+            self.refreshing.value = false
+            
+            guard let imageName = self.details.value?.imageName else { return }
+            self.dataManager.getHotelImage(imageName: imageName) { result in
+                switch result {
+                case .success(let image):
+                    self.image.value = image
+                case .failure(let error):
+                    self.error.value = error
+                }
+            }
+        }
+    }
+}
+
